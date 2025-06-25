50f5282e1ae72411225ec84e18a322d63652709d
Legivok <148662811+Legivok@users.noreply.github.com>
2025-06-25 09:51:05 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
-setInterval(() => {
-  location.reload();
-}, 5000);
+<button onclick="reloadWithSound()">Reload with sound</button>
+
+<script>
+function reloadWithSound() {
+  const audio = new Audio("https://www.soundjay.com/button/beep-07.mp3");
+  audio.play().then(() => {
+    setTimeout(() => {
+      location.reload();
+    }, 1000); // várunk, amíg lemegy a hang
+  }).catch((e) => {
+    console.error("Nem lehetett lejátszani:", e);
+    location.reload();
+  });
+}
+</script>
